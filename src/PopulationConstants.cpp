cc0a6239ffc978dfad1b9f6935a72476a2320f65
William Waites <wwaites@tardis.ed.ac.uk>
2017-02-28 17:50:56 +0000

    optogenetics runner, now with confirgurable mechanical properties

---
+++ src/PopulationConstants.cpp
@@ -0,0 +1,8 @@
+#include "PopulationConstants.hpp"
+#include "DifferentiatedCellProliferativeType.hpp"
+#include "DefaultCellProliferativeType.hpp"
+
+boost::shared_ptr<AbstractCellProperty> p_wild_type(new DefaultCellProliferativeType());
+boost::shared_ptr<AbstractCellProperty> p_diff_type(new DifferentiatedCellProliferativeType());
+double wild_type_lambda = 0.12;
+double diff_type_lambda = 0.12;
---
+++ src/PopulationConstants.hpp
@@ -0,0 +1,12 @@
+#ifndef POPULATION_CONSTANTS_HPP
+#define POPULATION_CONSTANTS_HPP
+
+#include "AbstractCellProliferativeType.hpp"
+
+extern boost::shared_ptr<AbstractCellProperty> p_wild_type;
+extern boost::shared_ptr<AbstractCellProperty> p_diff_type;
+
+extern double wild_type_lambda;
+extern double diff_type_lambda;
+
+#endif /* POPULATION_CONSTANTS_HPP */
